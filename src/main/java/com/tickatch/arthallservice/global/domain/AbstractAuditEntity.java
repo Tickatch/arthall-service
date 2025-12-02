5533330b729b50af0e4fe80a357807a90c24cf13
devangelwings <khs96523@gmail.com>
2025-12-02 16:03:52 +0900

    CHORE: 프로젝트 세팅

--- src/main/java/com/tickatch/arthallservice/global/domain/AbstractAuditEntity.java
+++ src/main/java/com/tickatch/arthallservice/global/domain/AbstractAuditEntity.java
@@ -1,4 +1,4 @@
-package com.tickatch.projectinterface.global.domain;
+package com.tickatch.arthallservice.global.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
