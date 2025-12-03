31f502cf2b56ae5b02c4a2fc8d0639cee7810080
nineeko <gkxm1013@naver.com>
2025-12-03 15:32:17 +0900

    FEAT: DDL 추가
    See also: #8

--- init.sql
+++ init.sql
@@ -1,5 +1,8 @@
 CREATE SCHEMA IF NOT EXISTS arthall_service;
+
+-- p_arthall
 DROP TABLE IF EXISTS arthall_service.p_arthall CASCADE;
+
 CREATE TABLE arthall_service.p_arthall
 (
     id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -17,4 +20,30 @@ CREATE TABLE arthall_service.p_arthall
 
     deleted_at TIMESTAMP,
     deleted_by VARCHAR(100)
+);
+
+-- ENUM 타입 (스키마 명시)
+CREATE TYPE arthall_service.stage_status AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- p_stage
+DROP TABLE IF EXISTS arthall_service.p_stage CASCADE;
+
+CREATE TABLE arthall_service.p_stage
+(
+    id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    arthall_id BIGINT                       NOT NULL
+        REFERENCES arthall_service.p_arthall (id),
+
+    name       VARCHAR(50)                  NOT NULL,
+    status     arthall_service.stage_status NOT NULL,
+
+    created_at TIMESTAMP                    NOT NULL DEFAULT NOW(),
+    created_by VARCHAR(100)                 NOT NULL,
+
+    updated_at TIMESTAMP                    NOT NULL DEFAULT NOW(),
+    updated_by VARCHAR(100)                 NOT NULL,
+
+    deleted_at TIMESTAMP                    NULL,
+    deleted_by VARCHAR(100)                 NULL
 );
