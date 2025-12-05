186e1ea8ba8fe5e54844ee263c6bb35f31f9c7a9
nineeko <gkxm1013@naver.com>
2025-12-05 13:36:52 +0900

    FEAT: 스테이지 좌석 조회 방법 수정 및 StageId에 해당하는 StageSeat 전체 조회 서비스 추가
    See also: #21

--- src/main/java/com/tickatch/arthallservice/stageseat/domain/repository/StageSeatQueryRepository.java
+++ src/main/java/com/tickatch/arthallservice/stageseat/domain/repository/StageSeatQueryRepository.java
@@ -1,13 +1,14 @@
 package com.tickatch.arthallservice.stageseat.domain.repository;
 
 import com.tickatch.arthallservice.stageseat.domain.StageSeat;
+import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface StageSeatQueryRepository {
 
   Optional<StageSeat> findDetailByStageSeatId(Long seatId);
 
-  Page<StageSeat> findByStageIdAndKeyword(Long stageId, String keyword, Pageable pageable);
+  List<StageSeat> findByStageIdAndSeatNumberLike(Long stageId, String seatNumber);
+
+  List<StageSeat> findAllByStageId(Long stageId);
 }
--- src/main/java/com/tickatch/arthallservice/stageseat/presentation/dto/response/StageSeatListResponse.java
+++ src/main/java/com/tickatch/arthallservice/stageseat/presentation/dto/response/StageSeatListResponse.java
@@ -1,11 +1,20 @@
 package com.tickatch.arthallservice.stageseat.presentation.dto.response;
 
 import com.tickatch.arthallservice.stageseat.application.dto.StageSeatListResult;
-import org.springframework.data.domain.Page;
+import java.util.List;
 
-public record StageSeatListResponse(Page<StageSeatListResult> content) {
+public record StageSeatListResponse(List<StageSeatListResult> content) {
 
-  public static StageSeatListResponse from(Page<StageSeatListResult> page) {
-    return new StageSeatListResponse(page);
+  public StageSeatListResponse {
+    content = List.copyOf(content);
+  }
+
+  public static StageSeatListResponse from(List<StageSeatListResult> list) {
+    return new StageSeatListResponse(list);
+  }
+
+  @Override
+  public List<StageSeatListResult> content() {
+    return content;
   }
 }
